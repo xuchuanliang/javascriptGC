cfe2a1b4bb45656f25b88036214abd57b0136a49
snail <xuchuanliang361@163.com>
2018-05-04 08:35:41 +0800

    javascript高级程序设计capter04

---
+++ javascriptGC/capter04/caper04.js
@@ -0,0 +1,29 @@
+// var color = "red";
+// //函数changeColor是一个作用域，由arguments变量，他的上一级作用域链是windows
+// function changeColor(){
+//     if(color=="blue"){
+//         color = "red";
+//     }else{
+//         color = "blue";
+//     }
+// }
+// changeColor();
+// console.log(color);
+// changeColor();
+// console.log(color);
+
+// var color = "blue";
+// // function chain1(){
+// //     var tempColor = "red";
+// //     function chain2(){
+// //         var chain2Color = "black";
+// //         chain2Color = tempColor;
+// //         color = chain2Color;
+// //     }
+// //     chain2();
+// //     //chain2Color 访问不到
+// // }
+// // chain1();
+// // //chain2() 访问不到
+
+
